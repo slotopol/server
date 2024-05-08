dbd60847a3a3f259f7af8303b4aadf3a0eb37fec
schwarzlichtbezirk <schwarzlichtbezirk@gmail.com>
2024-05-08 05:16:22 +0300

    panthermoon game added.

---
+++ config/links/panthermoon_link.go
@@ -0,0 +1,39 @@
+//go:build !prod || full || novomatic
+
+package links
+
+import (
+	"context"
+
+	"github.com/slotopol/server/game/panthermoon"
+	"github.com/spf13/pflag"
+)
+
+func init() {
+	FlagsSetters = append(FlagsSetters, func(flags *pflag.FlagSet) {
+		flags.Bool("panthermoon", false, "'Panther Moon' Novomatic 5x3 slots")
+	})
+	ScatIters = append(ScatIters, func(flags *pflag.FlagSet, ctx context.Context) {
+		if is, _ := flags.GetBool("panthermoon"); is {
+			var rn, _ = flags.GetString("reels")
+			if rn == "bon" {
+				panthermoon.CalcStatBon(ctx)
+			} else {
+				panthermoon.CalcStatReg(ctx, rn)
+			}
+		}
+	})
+
+	for _, alias := range []string{
+		"panthermoon",
+	} {
+		GameAliases[alias] = "panthermoon"
+	}
+
+	GameFactory["panthermoon"] = func(rd string) any {
+		if _, ok := panthermoon.ReelsMap[rd]; ok {
+			return panthermoon.NewGame(rd)
+		}
+		return nil
+	}
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/slotopol/server
 go 1.22
 
 require (
-	github.com/gin-gonic/gin v1.9.1
+	github.com/gin-gonic/gin v1.10.0
 	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/mattn/go-sqlite3 v1.14.22
 	github.com/spf13/cobra v1.8.0
