00bf0bc1ed73babc6f2899f23892d5a3b27e432e
schwarzlichtbezirk <schwarzlichtbezirk@gmail.com>
2024-09-20 21:24:15 +0300

    tikiwonders game added.

---
+++ config/links/tikiwonders_link.go
@@ -0,0 +1,36 @@
+//go:build !prod || full || netent
+
+package links
+
+import (
+	"context"
+
+	slot "github.com/slotopol/server/game/slot/tikiwonders"
+	"github.com/spf13/pflag"
+)
+
+func init() {
+	var gi = GameInfo{
+		Aliases: []GameAlias{
+			{"tikiwonders", "Tiki Wonders"},
+			{"geishawonders", "Geisha Wonders"},
+		},
+		Provider: "NetEnt",
+		ScrnX:    5,
+		ScrnY:    3,
+		RtpList:  MakeRtpList(slot.ReelsMap),
+	}
+	GameList = append(GameList, gi)
+
+	for _, ga := range gi.Aliases {
+		ScanIters = append(ScanIters, func(flags *pflag.FlagSet, ctx context.Context) {
+			if is, _ := flags.GetBool(ga.ID); is {
+				var rn, _ = flags.GetString("reels")
+				slot.CalcStatReg(ctx, rn)
+			}
+		})
+		GameFactory[ga.ID] = func() any {
+			return slot.NewGame()
+		}
+	}
+}
